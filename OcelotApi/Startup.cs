399119c960f7f52f7e2ae5df9e4d9248003aae1d
Lincoln Pires <me@lincolnpires.com>
2018-07-27 09:12:29 -0300

    add initial gateway

---
+++ OcelotApi/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            const string ocelotConfig = "ocelot.json";
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile(ocelotConfig)
+                .AddConfiguration(configuration);
+
+            Configuration = configurationBuilder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOcelot().Wait();
+        }
+    }
+}
