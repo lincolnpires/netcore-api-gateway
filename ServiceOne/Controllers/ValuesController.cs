5c062331037df7c74dadcd6bb7a77b92a9d77011
Lincoln Pires <me@lincolnpires.com>
2018-07-27 09:15:58 -0300

    adds service one

---
+++ ServiceOne/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ServiceOne.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ServiceOne value1", "ServiceOne value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "ServiceOne value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            // Method intentionally left empty.
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            // Method intentionally left empty.
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            // Method intentionally left empty.
+        }
+    }
+}
